a02dd7611856491310baabfba4a55783d579403f
jberthiaume1138 <joenyc23@gmail.com>
2016-08-29 02:19:10 -0400

    successful test pulling series data from Marvel API

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var app = express();
 
+var crypto = require('crypto-js');
+
+var unirest = require('unirest');
+
 app.get('/', function(request, response) {
     response.send('Hello, everyone');
     response.status(200);
@@ -12,8 +16,34 @@ app.get('/collection', function(request,response) {
 });
 
 app.get('/series', function(request,response) {
-    response.send('Details for a series.');
+    // response.send('Details for a series.');
     response.status(200);
+
+    var pub = '41ac4a2096f2416eb7cc039c19a371b3';
+    var priv = '37b4c45a10b08d114e19400a278f8fb52b9d0f79';
+
+    var ts = new Date().getTime();
+
+    var hash = crypto.MD5(ts + priv + pub).toString();
+
+    console.log(hash);
+
+    var url = 'http://gateway.marvel.com:80/v1/public/series/';
+    var seriesId = 20617;
+
+    var params = {  "apikey": pub,
+                    "ts": ts,
+                    "hash": hash
+                };
+
+    // unirest.get(url + seriesId + '/comics?ts=' + ts + 'apikey=' + pub + 'hash=' + hash)
+    unirest.get(url + seriesId + '/comics')
+            .qs(params)
+            .end(function(data) {
+                console.log(data.body);
+                response.send(data.body);
+            });
+
 });
 
 app.get('/shopping', function(request,response) {
