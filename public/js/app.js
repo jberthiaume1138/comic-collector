cc1ebb5fdf70563bd69b8ca5b0b37df6a8d49a15
jberthiaume1138 <joenyc23@gmail.com>
2016-10-10 12:30:16 -0400

    seties page formatting

--- public/js/app.js
+++ public/js/app.js
@@ -6,4 +6,16 @@ $('document').ready(function() {
     //     .done(function(data) {
     //         console.log(data);
     //     });
+    $('main').on('click','.button-need', function() {
+        console.log('need clicked');
+        console.log(this);
+    });
+
+    $('main').on('click','.button-own', function() {
+        console.log('own clicked');
+    });
+
+    Handlebars.registerHelper('dateTimeFormat', function(date, format) {
+        return moment(date).format(format);
+    });
 });
