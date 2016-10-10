90c0a0c2619ea4c1a14eb61171774ba7a3fa4c1d
jberthiaume1138 <joenyc23@gmail.com>
2016-10-10 00:08:22 -0400

    create app_server, sub dirs, move app routes to app_server/routes

--- app.js
+++ app.js
@@ -4,13 +4,14 @@ var app = express();
 var util = require('util');
 var path = require('path');
 
+// this can move
 var crypto = require('crypto-js');
 var unirest = require('unirest');
 
 if (!process.env.priv) {
     var env = require('./env.js');
 }
-
+//end of this
 var staticPath = path.join(__dirname,'public');
 app.use(express.static(staticPath));
 
@@ -18,47 +19,13 @@ var exphbs = require('express-handlebars');
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
-app.get('/', function(req, res) {
-    res.render('home');
-    res.status(200);
-});
+var routes = require('./app_server/routes/routes');
 
-app.get('/collection', function(req, res) {
-    res.render('collection');
-});
+app.use('/', routes);
 
-app.get('/series/:id', function(req,res) {
-    var pub = process.env.pub;
-    var priv = process.env.priv;
-
-    var ts = new Date().getTime();
-
-    var hash = crypto.MD5(ts + priv + pub).toString();
-
-    var url = 'http://gateway.marvel.com:80/v1/public/series/';
-    var seriesId = req.params.id;
-
-    var params = {  "apikey": pub,
-                    "ts": ts,
-                    "hash": hash
-                };
-
-    unirest.get(url + seriesId + '/comics')
-            .qs(params)
-            .end(function(data) {
-                res.render('series', data.body);
-                console.log(data.body);
-                // res.json(data.body);
-                // res.status(200);
-            });
-});
-
-app.get('/shop', function(req,res) {
-    res.render('shop');
-});
-
-app.get('/search', function(req,res) {
-    res.render('search');
+app.use('*', function(req, res) {
+    res.send('Error 404 - page not found!');
+    res.status(404);
 });
 
 app.listen(process.env.PORT || 8080);
---
+++ app_server/routes/routes.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+
+// pages
+router.get('/', function(req, res) {
+    res.render('home');
+});
+
+router.get('/collection', function(req, res) {
+    res.render('collection');
+});
+
+router.get('/shop', function(req,res) {
+    res.render('shop');
+});
+
+router.get('/search', function(req,res) {
+    res.render('search');
+});
+
+module.exports = router;
