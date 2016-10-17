f14b6a7166fe6b4bc32a316170fc3222cc2de2ae
jberthiaume1138 <joenyc23@gmail.com>
2016-10-17 02:06:23 -0400

    add hard coded json for a single series for styling ease

--- app_server/controllers/items.js
+++ app_server/controllers/items.js
@@ -4,26 +4,30 @@ var crypto = require('crypto-js');
 module.exports.renderSeries = function(req, res) {
     // gets the complete run of a series from Marvel's API
 
-    var pub = process.env.pub;
-    var priv = process.env.priv;
+    // var pub = process.env.pub;
+    // var priv = process.env.priv;
+    //
+    // var ts = new Date().getTime();
+    //
+    // var hash = crypto.MD5(ts + priv + pub).toString();
+    //
+    // var url = 'http://gateway.marvel.com:80/v1/public/series/';
+    // var seriesId = req.params.id;
+    //
+    // var params = {  "apikey": pub,
+    //                 "ts": ts,
+    //                 "hash": hash
+    //             };
+    //
+    // unirest.get(url + seriesId + '/comics')
+    //         .qs(params)
+    //         .end(function(data) {
+    //             res.render('series', data.body);
+    //         });
 
-    var ts = new Date().getTime();
-
-    var hash = crypto.MD5(ts + priv + pub).toString();
-
-    var url = 'http://gateway.marvel.com:80/v1/public/series/';
-    var seriesId = req.params.id;
-
-    var params = {  "apikey": pub,
-                    "ts": ts,
-                    "hash": hash
-                };
-
-    unirest.get(url + seriesId + '/comics')
-            .qs(params)
-            .end(function(data) {
-                res.render('series', data.body);
-            });
+    // **** temp hard coding data for a series ****
+    var logan = require('../data/logan.json');
+    res.render('series', logan);
 };
 
 module.exports.renderCollection = function(req, res) {
