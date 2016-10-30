94bcb94f0b9fa98ee4b5c0efe382196e9db9c873
jberthiaume1138 <joenyc23@gmail.com>
2016-10-30 00:24:40 -0400

    Gulp settings for handlebars

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,15 @@ gulp.task('compile-styles', function() {
         .pipe(gulp.dest('./public/css/'));
 });
 
+// var handlebars = require('gulp-handlebars');
+//
+// gulp.task('compile-handlebars', function() {
+//     gulp.src('./app_server/views/precompile/*.handlebars')
+//         .pipe(handlebars())
+//         .pipe(gulp.dest('./public/js/templates.js'))
+// });
+
 gulp.task('default', function() {
     gulp.watch('./public/sass/**/*.scss',['compile-styles']);
+    // gulp.watch('./app_server/views/precompile/*.handlebars',['compile-handlebars'])
 });
