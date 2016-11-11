909c7d7cdd1086e0d13a42da68a1459462cb9ab0
jberthiaume1138 <joenyc23@gmail.com>
2016-11-11 02:13:46 -0500

    Debugging Heroku database config - logic

--- api/db/connect.js
+++ api/db/connect.js
@@ -26,9 +26,6 @@ else {
     console.log(dbURI);
 }
 
-// var dbURI = 'mongodb://heroku_8k30n99h:d646ehdetp8gn6egficncpdulm@ds149567.mlab.com:49567/heroku_8k30n99h';
-
-
 mongoose.connect(dbURI);
 
 // CONNECTION EVENTS
